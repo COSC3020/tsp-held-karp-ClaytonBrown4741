b72d8ed87ee3af70c06e973fa7e0fc309eebdce9
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-12-06 17:06:51 -0700

    Updated Held-Karp to be more efficient

--- code.js
+++ code.js
@@ -1,51 +1,51 @@
 function tsp_hk(distance_matrix) {
-	cache=[]
+	var cache={}
+	//cache.splice(0,cache.length)
 	var nodesLeft = [];
 	var minimumValue = Infinity
 	var bufferVar=-1
  	if(distance_matrix.length < 2) {
         	return 0;
-    	} 
+    	}
 	cities=Object.keys(distance_matrix)
         for(var i = 0; i < distance_matrix.length; i++){
-            	bufferVar = tsp_HeldKarp(distance_matrix, i, cities) 
+            	//bufferVar = tsp_HeldKarp(distance_matrix, i, cities)
+		bufferVar=tsp_HeldKarp(distance_matrix, i, cities, cache)
 		if (bufferVar < minimumValue)
-                	minimumValue = bufferVar 
+                	minimumValue = bufferVar
     	}
     	return minimumValue
 }
 
-function tsp_HeldKarp(distance_matrix, start, cities) {
-	if(cities.length < 1) {
-		return 0;
-        }
-	var key = JSON.stringify(start+cities)
-    	if(cache[key] !== undefined) {
-		//console.log("cache being accessed")
-		return cache[key]
+function tsp_HeldKarp(distances, start, cities, cache)
+{
+	var key = JSON.stringify(cities) + start
+	if (cache[key] === undefined){
+		cache[key] = {};
 	}
-	if(cities.length == 1) {
-          	cache[key] = distance_matrix[start][cities[0]];
-            	return cache[key];
-        }
-	var minimumVal = Infinity
-	var desiredIndex = -1
-	var originalValue=0
-        for(var i = 0; i < cities.length; i++) {
-                originalValue = cities.splice(i,1)
-		//console.log(tempStart)
-                buffer = tsp_HeldKarp(distance_matrix, originalValue, cities)
-		//console.log(temp)
-                if(buffer < minimumVal) {
-                    	minimumVal = buffer;
-			desiredIndex = originalValue;
-                }
-                cities.splice(i,0,originalValue);
-        }                                                                          
-        cache[key] = minimumVal + distance_matrix[start][desiredIndex];
-	//console.log(cache[key])
-	//console.log("cache not being used")
-        return cache[key];                  
-}
+        else{ 
+		return cache[key];
+	}
 
-cache=[]
+    	if (cities.length <= 1)
+    	{
+        	cache[key] = distances[start][cities[0]]
+        	return distances[start][cities[0]];
+    	}
+    	else
+    	{
+        	var minimumDistance = Infinity;
+
+        	for (var i = 0; i < cities.length; i++)
+        	{
+            		var newCities = cities.filter(city => city != start)
+            		var sumDist = distances[start][cities[i]] + tsp_HeldKarp(distances, cities[i], newCities, cache);
+			if (sumDist < minimumDistance){
+				minimumDistance=sumDist
+			}
+        	}
+
+        	cache[key] = minimumDistance;
+        	return minimumDistance;
+    	}
+}
